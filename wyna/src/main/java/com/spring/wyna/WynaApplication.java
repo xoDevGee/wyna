62ec41fd403a9a675fe5f0755f5cbda7c52effe3
SirGonny <rosevirus.k@gmail.com>
2021-06-09 00:53:04 +0900

    [001 - add spring]

---
+++ wyna/src/main/java/com/spring/wyna/WynaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.wyna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WynaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WynaApplication.class, args);
+	}
+
+}
---
+++ wyna/src/test/java/com/spring/wyna/WynaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.wyna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WynaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
