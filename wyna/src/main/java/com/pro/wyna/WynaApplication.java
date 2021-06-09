e4327d1975d6b71693749da9e157b3fd23614644
SirGonny <rosevirus.k@gmail.com>
2021-06-09 23:31:04 +0900

    [002 - maven > gradle]

--- wyna/src/main/java/com/pro/wyna/WynaApplication.java
+++ wyna/src/main/java/com/pro/wyna/WynaApplication.java
@@ -1,4 +1,4 @@
-package com.spring.wyna;
+package com.pro.wyna;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- wyna/src/test/java/com/pro/wyna/WynaApplicationTests.java
+++ wyna/src/test/java/com/pro/wyna/WynaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.spring.wyna;
+package com.pro.wyna;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
